c42f1760baf674d08dd85d8a94efcc81dae48a8f
verozroke <73778760+verozroke@users.noreply.github.com>
2023-06-17 20:25:28 +0600

    workflow added due to purpose to deploy it on gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nomad-it/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
