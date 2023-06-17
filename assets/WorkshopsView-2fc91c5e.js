4fad337ed17e3eba318f6ff5e2d662c483e2b815
verozroke <verozroke@users.noreply.github.com>
2023-06-17 14:26:11 +0000

    deploy: c42f1760baf674d08dd85d8a94efcc81dae48a8f

---
+++ assets/WorkshopsView-2fc91c5e.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,g as e}from"./index-facf558f.js";const c={},r={class:"workshops"};function t(n,_){return s(),e("div",r," workshops ")}const p=o(c,[["render",t]]);export{p as default};
