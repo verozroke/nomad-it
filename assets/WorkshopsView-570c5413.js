7480e866583eb306ec36cb825d6f0b0349af4ebd
verozroke <verozroke@users.noreply.github.com>
2023-06-17 14:39:10 +0000

    deploy: 7977770cee5099101fc7ceff79803dc35c26f014

---
+++ assets/WorkshopsView-570c5413.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,g as e}from"./index-0d05736f.js";const c={},r={class:"workshops"};function t(n,_){return s(),e("div",r," workshops ")}const p=o(c,[["render",t]]);export{p as default};
